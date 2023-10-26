ec4ccb0791bd7938a8182ce5178588d152c07956
Eunsong Koh <eunsongkohh@gmail.com>
2023-10-26 16:40:14 -0700

    yes fuinbally

--- profile2.0/src/pages/home.js
+++ profile2.0/src/pages/home.js
@@ -1,8 +1,7 @@
 import classes from "./home.module.css";
 import { Element, scroller } from "react-scroll";
 import Navbar from "../components/Layout/navbar";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { Chrono } from "react-chrono";
 
 const ScrollToTopButton = () => {
   const scrollToTop = () => {
@@ -20,6 +19,37 @@ const ScrollToTopButton = () => {
 };
 
 const HomePage = () => {
+  const items = [
+    {
+      title: "Gesture Genius, Hack the North",
+      cardTitle: "HTN",
+      url: "https://devpost.com/software/gesture-genius-asl-ai-recognition",
+      cardSubtitle:
+        "An accessible ASL Translator utilizing Google Cloud's API and Teachable Machine AI Model",
+      cardDetailedText: "HTML/CSS, JS, Ajax, Node.js, Google Cloud API",
+      media: {
+        type: "IMAGE",
+        source: {
+          media: "/img/gesture.png",
+        },
+      },
+    },
+    {
+      title: "SFU Developers & Systems Club Official Website",
+      cardTitle: "SFU DNS Club",
+      url: "https://dnsclub.github.io/dnsWeb/",
+      cardSubtitle:
+        "The official website of SFU DNS Club website created with React.js",
+      cardDetailedText: "React.js, HTML/CSS",
+      media: {
+        type: "IMAGE", // You can specify other media types if needed
+        source: {
+          media: "DNSPoster.png", // Replace with the actual image URL
+        },
+      },
+    },
+  ];
+
   return (
     <main>
       <Element name="intro">
@@ -34,7 +64,11 @@ const HomePage = () => {
       </Element>
       <div className={classes.projects} id="projects">
         <h2>My Projects</h2>
-        <section className={classes.theCards}></section>
+        <section className={classes.theCards}>
+          <div style={{ width: "100%", height: "100%" }}>
+            <Chrono items={items} mode="HORIZONTAL" />
+          </div>
+        </section>
       </div>
       <div className={classes.multimedia} id="multimedia">
         <h2>multimedia</h2>
