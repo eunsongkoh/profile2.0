de0cc99e10de71569d6dbab2e44403311b8b8cfc
Eunsong Koh <eunsongkohh@gmail.com>
2023-10-26 17:05:06 -0700

    Added Projects

--- profile2.0/src/pages/home.js
+++ profile2.0/src/pages/home.js
@@ -21,8 +21,28 @@ const ScrollToTopButton = () => {
 const HomePage = () => {
   const items = [
     {
-      title: "Gesture Genius, Hack the North",
-      cardTitle: "HTN",
+      title: "Personal Website",
+      cardTitle: "Personal Website",
+      cardSubtitle:
+        "What you're looking at right now! I love learning and challenging myself with new skills. This website will be a real-time update of where I am in my coding journey!",
+      cardDetailedText: "React.js",
+    },
+    {
+      title: "Every Second Devotion, 2023",
+      cardTitle: "Every Second Devotion",
+      cardSubtitle:
+        "A devotional web app that generates descriptions of any Bible verse. Created as a birthday gift! ",
+      cardDetailedText: "Next.js, Express.js, Node.js",
+      media: {
+        type: "IMAGE",
+        source: {
+          media: "",
+        },
+      },
+    },
+    {
+      title: "Gesture Genius, Hack the North 2023",
+      cardTitle: "Gesture Genius, Hack the North 2023",
       url: "https://devpost.com/software/gesture-genius-asl-ai-recognition",
       cardSubtitle:
         "An accessible ASL Translator utilizing Google Cloud's API and Teachable Machine AI Model",
@@ -35,8 +55,8 @@ const HomePage = () => {
       },
     },
     {
-      title: "SFU Developers & Systems Club Official Website",
-      cardTitle: "SFU DNS Club",
+      title: "SFU Developers & Systems Club Official Website 2023",
+      cardTitle: "SFU DNS Club Website",
       url: "https://dnsclub.github.io/dnsWeb/",
       cardSubtitle:
         "The official website of SFU DNS Club website created with React.js",
@@ -44,12 +64,64 @@ const HomePage = () => {
       media: {
         type: "IMAGE", // You can specify other media types if needed
         source: {
-          media: "DNSPoster.png", // Replace with the actual image URL
+          media: "img/DNSPoster.png", // Replace with the actual image URL
+        },
+      },
+    },
+    {
+      title: "AutoFill PDF 2023",
+      cardTitle: "AutoFill PDF",
+      cardSubtitle:
+        "A Python program developed to automate onboarding contracts for NCI HR Department",
+      cardDetailedText: "Python",
+      media: {
+        type: "IMAGE", // You can specify other media types if needed
+        source: {
+          media: "img/pdf.jpg", // Replace with the actual image URL
+        },
+      },
+    },
+    {
+      title: "Weather App 2022",
+      cardTitle: "Weather App",
+      url: "https://github.com/eunsongkoh/weather-app.py",
+      cardSubtitle:
+        "An application that provides real-time updates on weather conditions on user-inputted cities",
+      cardDetailedText: "Python, Json, Requests, Open-Weather API, Tkinter GUI",
+      media: {
+        type: "IMAGE", // You can specify other media types if needed
+        source: {
+          media: "img/password.png", // Replace with the actual image URL
+        },
+      },
+    },
+    {
+      title: "Password Manager 2022",
+      cardTitle: "Password Manager",
+      url: "https://github.com/eunsongkoh/passwordVault.py",
+      cardSubtitle: "A Python application that stores a database of passwords",
+      cardDetailedText: "Python, SQLite3, Tkinter, HashLib",
+      media: {
+        type: "IMAGE", // You can specify other media types if needed
+        source: {
+          media: "./img/DNSPoster.png", // Replace with the actual image URL
+        },
+      },
+    },
+    {
+      title: "Kode with Klossy Data Science Scholar 2022",
+      cardTitle: "KWK Data Science Scholar 2022",
+      cardSubtitle:
+        "Utilized Python and SQL to analyze global and national data on violence against women and provided insights into patterns and trends on a web application",
+      cardDetailedText: "SQL, Python, Google Data Studio",
+      media: {
+        type: "IMAGE", // You can specify other media types if needed
+        source: {
+          media: "./img/kwk.jpg", // Replace with the actual image URL
         },
       },
     },
   ];
-
   return (
     <main>
       <Element name="intro">
@@ -66,7 +138,17 @@ const HomePage = () => {
         <h2>My Projects</h2>
         <section className={classes.theCards}>
           <div style={{ width: "100%", height: "100%" }}>
-            <Chrono items={items} mode="HORIZONTAL" />
+            <Chrono
+              items={items}
+              theme={{
+                primary: "#8693AB",
+                secondary: "#8693AB",
+                titleColor: "white",
+                titleColorActive: "#F4D6CC",
+                cardTitleColor: "#8693AB",
+              }}
+              mode="HORIZONTAL"
+            />
           </div>
         </section>
       </div>
